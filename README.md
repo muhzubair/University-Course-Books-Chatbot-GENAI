1e90ba9bef82df900165f920f94eb2da324fe3ef
Adeel Zubair <adeel@Adeels-MacBook-Air.local>
2025-04-11 16:01:29 -0500

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # University-Course-Books-Chatbot-GENAI
+
+### This project will help me review the data science conepts I leared in University from different course books
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI CourseBook Project',
+    version= '0.0.0',
+    author= 'Muhammad Zubair',
+    author_email= 'adeelzubair2001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
